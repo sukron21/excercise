77a6f7355e46b735a2cdbc5f9c36e66d5474bdb7
lol <Rahmatfurqon0@gmail.com>
2022-09-17 15:25:35 +0700

    Update commnet.router.js

--- src/router/commnet.router.js
+++ src/router/commnet.router.js
@@ -1,7 +1,7 @@
 // deklare exprees
 const express = require('express')
 // D:\belajar pijar\excercise_beginner_backend\src\controller\comment.controller.js
-const { list, destroy, detail, insert, update, detailofname } = require('../controller/comment.controller')
+const { list, destroy, detail, insert, update, detailofname } = require('../controller/comment.controller.js')
 
 const router = express.Router()
 
