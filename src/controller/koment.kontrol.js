af501ea834264d957cb787f203fb36fd2b9327b5
lol <Rahmatfurqon0@gmail.com>
2022-09-18 09:30:19 +0700

    Update koment.kontrol.js

--- src/controller/koment.kontrol.js
+++ src/controller/koment.kontrol.js
@@ -10,64 +10,64 @@ const komenController = {
       .catch((err) => {
         res.json(err)
       })
+  },
+  detail: (req, res) => {
+    const id = req.params.id
+    komenModel
+      .selectDetail(id)
+      .then((result) => {
+        res.json(result)
+      })
+      .catch((err) => {
+        res.json(err)
+      })
+  },
+  detailname: (req, res) => {
+    const id = req.params.id_recipe
+    komenModel
+      .nameDetail(id)
+      .then((result) => {
+        res.json(result)
+      })
+      .catch((err) => {
+        res.json(err)
+      })
+  },
+  insert: (req, res) => {
+    // eslint-disable-next-line camelcase
+    const { id, comments, id_user, id_recipe } = req.body
+    komenModel
+      .store(id, comments, id_user, id_recipe)
+      .then((result) => {
+        res.json('Account added successfully')
+      })
+      .catch((err) => {
+        res.json(err)
+      })
+  },
+  update: (req, res) => {
+    const id = req.params.id
+    // eslint-disable-next-line camelcase
+    const { comments, id_user, id_recipe } = req.body
+    komenModel
+      .updateAccount(id, comments, id_user, id_recipe)
+      .then((result) => {
+        res.json('Account Updated')
+      })
+      .catch((err) => {
+        res.json(err)
+      })
+  },
+  destroy: (req, res) => {
+    komenModel
+      .delete(req.params.id)
+      .then((result) => {
+        res.json('Account Deleted')
+      })
+      .catch((err) => {
+        res.json(err)
+      })
   }
-//   detail: (req, res) => {
-//     const id = req.params.id
-//     recipeModel
-//       .selectDetail(id)
-//       .then((result) => {
-//         res.json(result)
-//       })
-//       .catch((err) => {
-//         res.json(err)
-//       })
-//   },
-//   detailname: (req, res) => {
-//     const id = req.params.name_recipe
-//     recipeModel
-//       .nameDetail(id)
-//       .then((result) => {
-//         res.json(result)
-//       })
-//       .catch((err) => {
-//         res.json(err)
-//       })
-//   },
-//   insert: (req, res) => {
-//     // eslint-disable-next-line camelcase
-//     const { id, name_recipe, ingredients } = req.body
-//     recipeModel
-//       .store(id, name_recipe, ingredients)
-//       .then((result) => {
-//         res.json('Account added successfully')
-//       })
-//       .catch((err) => {
-//         res.json(err)
-//       })
-//   },
-//   update: (req, res) => {
-//     const id = req.params.id
-//     // eslint-disable-next-line camelcase
-//     const { name_recipe, ingredients } = req.body
-//     recipeModel
-//       .updateAccount(id, name_recipe, ingredients)
-//       .then((result) => {
-//         res.json('Account Updated')
-//       })
-//       .catch((err) => {
-//         res.json(err)
-//       })
-//   },
-//   destroy: (req, res) => {
-//     recipeModel
-//       .delete(req.params.id)
-//       .then((result) => {
-//         res.json('Account Deleted')
-//       })
-//       .catch((err) => {
-//         res.json(err)
-//       })
-//   }
 }
 
 module.exports = komenController
